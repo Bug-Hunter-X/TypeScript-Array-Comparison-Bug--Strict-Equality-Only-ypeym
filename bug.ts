94a1f8a9b4150060f3ab6cff8bdba64f4a98d1cb
BugHunter-X <askzenai@gmail.com>
2024-12-22 04:20:05 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,13 @@
+function compareArrays(arr1: number[], arr2: number[]): boolean {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false; //This is where the bug is
+    }
+  }
+
+  return true;
+}
